51347b70a75144ce34e35d5294bebcdc5eb3e32c
TiaSous <tiago.rodrigues2@eduvaud.ch>
2023-11-19 20:44:12 +0100

    feat: add a damage system and add font from official undertale game

--- rom_hack/Scripts/player.gd
+++ rom_hack/Scripts/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 250.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var life = 20
 
+@onready var timer = $Timer
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
 	
@@ -23,5 +24,18 @@ func _physics_process(delta):
 		velocity.y = directionY * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+		
 	move_and_slide()
+
+
+
+func _on_can_take_damage_area_entered(area):
+	if timer.is_stopped():
+		life -= 1
+		timer.start()
+		animated_sprite_2d.play("damage")
+
+
+func _on_timer_timeout():
+	timer.stop()
+	animated_sprite_2d.stop()
